8e356e442798c168fc3631db5ade54da88ba4896
Arthur White <arthurwhite@users.noreply.github.com>
2022-03-25 21:04:53 +0100

    Create run-reverse-proxy.sh

---
+++ run-reverse-proxy.sh
@@ -0,0 +1,40 @@
+EMAIL="hello@alefunion.com"
+
+CONFIG="global:
+  checkNewVersion: true
+  sendAnonymousUsage: false
+
+providers:
+  docker: {}
+
+api:
+  insecure: true
+
+entryPoints:
+  web:
+    address: :80
+
+  websecure:
+    address: :443
+
+certificatesResolvers:
+  letsEncrypt:
+    acme:
+      email: $EMAIL
+      storage: acme.json
+      httpChallenge:
+        entryPoint: web
+"
+
+echo "$CONFIG" > traefik.yml
+
+touch acme.json
+
+docker run -d \
+  --name reverse-proxy \
+  --restart unless-stopped \
+  -p 8080:8080 -p 80:80 -p 443:443 \
+  -v $PWD/traefik.yml:/etc/traefik/traefik.yml \
+  -v $PWD/acme.json:/acme.json \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  traefik
