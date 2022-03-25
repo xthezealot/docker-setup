4fd045e357202757c2b70a784f2617e4b3d5c925
Arthur White <arthurwhite@users.noreply.github.com>
2022-03-25 21:59:19 +0100

    Create run-web-service-template.sh

---
+++ run-web-service-template.sh
@@ -0,0 +1,13 @@
+NAME=""
+PORT=
+DOMAIN=""
+IMAGE=""
+
+docker run -d --rm \
+  --name $NAME \
+  -p $PORT:$PORT \
+  --label "traefik.http.routers.$NAME-http.rule=Host(\"$DOMAIN\")" \
+  --label "traefik.http.routers.$NAME-https.rule=Host(\"$DOMAIN\")" \
+  --label "traefik.http.routers.$NAME-https.tls=true" \
+  --label "traefik.http.routers.$NAME-https.tls.certresolver=letsEncrypt" \
+  $IMAGE
