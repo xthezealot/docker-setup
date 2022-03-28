02e3da4bcea5fbd97e5e2933b895f462585e7a63
Arthur White <arthurwhite@users.noreply.github.com>
2022-03-28 17:51:48 +0200

    Update run-registry.sh

--- run-registry.sh
+++ run-registry.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo "Domain name:"
+read DOMAIN
+
 docker run -d \
   --name registry \
   --restart unless-stopped \
   -p 5000:5000 \
+  --label "traefik.http.routers.registry-https.rule=Host(\"$DOMAIN\")" \
+  --label "traefik.http.routers.registry-https.tls=true" \
+  --label "traefik.http.routers.registry-https.tls.certresolver=letsEncrypt" \
   registry:2
