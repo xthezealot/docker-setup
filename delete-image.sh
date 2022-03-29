60446c861ca7f52da29947c3de3a2543d44f4a71
Arthur White <arthurwhite@users.noreply.github.com>
2022-03-29 04:20:14 +0200

    Create delete-image.sh

---
+++ delete-image.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "User:"
+read USER
+
+echo "Password:"
+read -s PASSWORD
+
+echo "Catalog:"
+curl -u $USER:$PASSWORD https://registry.alefunion.com/v2/_catalog
+
+echo "Docker image name to delete:"
+read IMAGE
+
+echo "Tag name:"
+read TAG
+
+REGISTRY="localhost:5000"
+
+curl $auth -X DELETE -sI -k "https://$REGISTRY/v2/$IMAGE/manifests/$(
+  curl -u $USER:$PASSWORD -sI -k \
+    -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+    "https://$REGISTRY/v2/$IMAGE/manifests/$TAG" \
+    | tr -d '\r' | sed -En 's/^Docker-Content-Digest: (.*)/\1/pi'
+)"
