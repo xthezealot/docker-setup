77625ef5b4f77a337828a6dcc1f232e0bcc303c2
Arthur White <arthurwhite@users.noreply.github.com>
2022-03-29 02:49:04 +0200

    Update run-registry.sh

--- run-registry.sh
+++ run-registry.sh
@@ -3,10 +3,23 @@
 echo "Domain name:"
 read DOMAIN
 
+echo "User:"
+read USER
+
+echo "Password:"
+read -s PASSWORD
+
+docker run -rm --entrypoint htpasswd httpd:2 -Bbn $USER $PASSWORD > htpasswd
+
 docker run -d \
   --name registry \
   --restart unless-stopped \
   -p 5000:5000 \
+  -v $PWD/htpasswd:/htpasswd \
+  -e REGISTRY_STORAGE_DELETE_ENABLED=true \
+  -e REGISTRY_AUTH=htpasswd \
+  -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
+  -e REGISTRY_AUTH_HTPASSWD_PATH=/htpasswd \
   --label "traefik.http.routers.registry-https.rule=Host(\"$DOMAIN\")" \
   --label "traefik.http.routers.registry-https.tls=true" \
   --label "traefik.http.routers.registry-https.tls.certresolver=letsEncrypt" \
