1e0e2bcf062087608e64dd529ff1187c300112cb
Arthur White <arthur@white.li>
2022-03-31 23:19:52 +0200

    Connect db-pg to own network

--- run-db-pg.sh
+++ run-db-pg.sh
@@ -10,6 +10,7 @@ docker volume create $CONTAINER
 docker run -d \
   --name $CONTAINER \
   --restart unless-stopped \
+  --network db-pg \
   -p 5432:5432 \
   -v $CONTAINER:/var/lib/postgresql/data \
   -e POSTGRES_PASSWORD=$PASSWORD \
